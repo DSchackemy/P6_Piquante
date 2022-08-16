5bbaf02a6e43ab89e254131d1da831e68964cfba
Damien Schackemy <damien.schackemy@gmail.com>
2022-08-16 18:50:16 +0200

    methode Put corrigé

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -36,30 +36,26 @@ exports.createSauce = (req, res, next) => {
 
 //Method PUT
 exports.modifySauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then((sauce) => {
-            if (!sauce) {
-            return res.status(404).json({error: "Not found"});
-            }
-        if (sauce.userId !== req.auth.userId) {
-            return res.status(401).json({error: "Requête non autorisée"});
-            }
-        const sauceObject = req.file
-            ? {
-                ...JSON.parse(req.body),
-                imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-            }
-            : req.body;
+    const sauceObject = req.file ? {
+        ...JSON.parse(req.body.thing),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+    } : {...req.body};
 
-        Sauce.updateOne({ _id: req.params.id }, sauceObject)
-            .then(() => res.status(200).json(req.body))
-            .catch((error) => res.status(400).json({ error }));
-        })
-        .catch((error) => {
-        console.log(error);
-        res.status(500).json({error});
-        });
-    };
+delete sauceObject._userId;
+Sauce.findOne({_id: req.params.id})
+    .then((sauce) => {
+        if (sauce.userId != req.auth.userId) {
+            res.status (401).json({message : 'Non-autorisé'});
+        } else {
+            Sauce.updateOne({ _id: req.params.id}, {...sauceObject, _id: req.params.id})
+            .then(() => res.status(200).json({messa: ' Objet modifié !'}))
+            .catch(error => { res.status(401).json ( {error})});
+        }
+    })
+    .catch((error => {
+        res.status(400).json({error});
+    }))
+};
 
 //Method DELETE
 exports.deleteSauce = (req, res, next) => {

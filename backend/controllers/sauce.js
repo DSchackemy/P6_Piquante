7bdf24bc6bce2b885c387fa93acfc651b91930f8
Damien Schackemy <damien.schackemy@gmail.com>
2022-08-26 14:46:55 +0200

    ajout de gitignore pour ignorer le dossier image

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -41,14 +41,20 @@ exports.modifySauce = (req, res, next) => {
     req.file ? (
     Sauce.findOne({_id: req.params.id})
         .then((sauce) => {
+        // On supprime l'ancienne image du serveur
         const filename = sauce.imageUrl.split('/images/')[1]
         fs.unlinkSync(`images/${filename}`)
         }),
-        sauceObject = {...JSON.parse(req.body.sauce),
+        sauceObject = {
+        // On modifie les données et on ajoute la nouvelle image
+        ...JSON.parse(req.body.sauce),
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-        }) : ( 
+        }
+    ) : ( // Opérateur ternaire équivalent à if() {} else {} => condition ? Instruction si vrai : Instruction si faux
+      // Si la modification ne contient pas de nouvelle image
         sauceObject = {...req.body}
     )
+    // On applique les paramètre de sauceObject
     Sauce.updateOne({_id: req.params.id}, {...sauceObject,_id: req.params.id})
         .then(() => res.status(200).json({message: 'Sauce modifiée !'}))
         .catch((error) => res.status(400).json({error}))
